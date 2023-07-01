40272d40d56249bfe05867503c8b8a1cc5843f17
DevonRem <DevonReminick@gmail.com>
2023-07-01 14:12:10 -0400

    rework code logic to work onscreen instead of in console

--- myscript.js
+++ myscript.js
@@ -15,52 +15,79 @@ function getComputerChoice() {
     }
 }
 
+
+
 function playRound(playerselection, computerSelection) {
     playerselection.toLowerCase();
     if (playerselection === "rock") {
         if (computerSelection === "scissor") {
-            return "you win! Rock beats scissor";
+            playerScore += 1;
+            message.textContent = "you win! Rock beats scissor";
         }
         else if (computerSelection === "paper") {
-            return "you lose! Paper beats rock";
+            computerScore += 1;
+            message.textContent = "you lose! Paper beats rock";
         }
         else {
-            return "Tie! Both of you chose rock";
+            message.textContent = "Tie! Both of you chose rock";
         }
     }
     if (playerselection === "scissor") {
         if (computerSelection === "scissor") {
-            return "Tie! Both of you chose scissor";
+            message.textContent = "Tie! Both of you chose scissor";
         }
         else if (computerSelection === "paper") {
-            return "you win! scissor beats paper";
+            playerScore += 1;
+            message.textContent = "you win! scissor beats paper";
         }
         else {
-            return "you lose! rock beats scissor";
+            computerScore += 1;
+            message.textContent = "you lose! rock beats scissor";
         }
     }
     if (playerselection === "paper") {
         if (computerSelection === "scissor") {
-            return "you lose! scissor beats paper";
+            computerScore += 1;
+            message.textContent = "you lose! scissor beats paper";
         }
         else if (computerSelection === "paper") {
-            return "Tie! both of you chose paper";
+            message.textContent = "Tie! both of you chose paper";
         }
         else {
-            return "you win! paper beats rock";
+            playerScore += 1;
+            message.textContent = "you win! paper beats rock";
         }
     }
 }
 
+/*  used in console version
 function game() {
     for (i = 0; i < 4; i++) {
         let playerselection = prompt("Please select rock, paper, or scissor");
         let computerSelection = getComputerChoice();
         console.log(playRound(playerselection,computerSelection));
-    }
+    } 
+
+
 }
+game(); */
 
-game();
+let playerScore = 0;
+let computerScore = 0;
 
+const message = document.querySelector('.message');
+const results = document.querySelector('.results');
+
+results.textContent = `Player: ${ playerScore }, Computer: ${ computerScore }`;
+results.innerHTML = `Player: ${ playerScore }, Computer: ${ computerScore }`;
+
+const rockbtn = document.querySelector('.rockbtn');
+rockbtn.addEventListener('click', () => playRound('rock',getComputerChoice()));
+
+const paperbtn = document.querySelector('.paperbtn');
+paperbtn.addEventListener('click', () => playRound('paper',getComputerChoice()));
+
+const scissorsbtn = document.querySelector('.scissorsbtn');
+scissorsbtn.addEventListener('click', () => playRound('scissor',getComputerChoice()));
 
 
